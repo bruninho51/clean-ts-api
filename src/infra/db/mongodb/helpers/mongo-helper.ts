6700c07c9fb4c2f19ab2bd721dc09b5526a9c569
Bruno Mendes <bruno.mendes51@hotmail.com>
2020-11-23 16:30:55 -0300

    refactor: adjust mongo helper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -8,22 +8,17 @@ export class MongoHelper {
   public static readonly instance: MongoHelper = new MongoHelper()
 
   async connect (uri: string): Promise<void> {
-    if (!this.client) {
-      this.client = await MongoClient.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      })
-    }
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
   }
 
   async disconnect (): Promise<void> {
-    if (this.client) {
-      await this.client.close()
-    }
+    await this.client.close()
   }
 
   async getCollection (name: string): Promise<Collection> {
-    await this.connect('')
     return this.client.db().collection(name)
   }
 }
