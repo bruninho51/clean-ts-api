1c67fc411011a5074e6ad11033461eb5dade4071
Bruno Mendes <bruno.mendes51@hotmail.com>
2020-11-25 17:59:45 -0300

    refactor: correct path files

--- src/presentations/controllers/login/login.spec.ts
+++ src/presentations/controllers/login/login.spec.ts
@@ -1,6 +1,5 @@
 import { LoginController } from './login'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
-import { MissingParamError } from '../../errors'
 import { HttpRequest, Authentication } from './login-protocols'
 import { Validation } from '../../helpers/validators'
 
