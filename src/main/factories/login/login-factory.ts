cacda1a1574d3f2525797e918190aab3c911e37b
Bruno Mendes <bruno.mendes51@hotmail.com>
2020-11-26 16:29:56 -0300

    refactor: change LoginFactory

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -3,22 +3,19 @@ import { Controller } from '../../../presentations/protocols'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { makeLoginValidation } from './login-validation-factory'
 import { LoginController } from '../../../presentations/controllers/login/login-controller'
-import { AuthenticateModel, Authentication } from '../../../domain/usecases/authentication'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
+import env from '../../config/env'
 
-const makeAuthenticateStub = (): Authentication => {
-  class AuthenticationStub implements Authentication {
-    async auth (authentication: AuthenticateModel): Promise<string> {
-      return 'any_token'
-    }
-  }
-
-  return new AuthenticationStub()
-}
-
-export const makeSignUpController = (): Controller => {
-  const validationComposite = makeLoginValidation()
-  const authenticator = makeAuthenticateStub()
-  const loginController = new LoginController(authenticator, validationComposite)
+export const makeLoginController = (): Controller => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+  const loginController = new LoginController(dbAuthentication, makeLoginValidation())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(loginController, logMongoRepository)
 }
