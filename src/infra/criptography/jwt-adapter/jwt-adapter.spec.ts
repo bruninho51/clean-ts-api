320d9f112a47a17f90dd7d71865db53bcfdb01c9
Bruno Mendes <bruno.mendes51@hotmail.com>
2020-11-26 13:49:25 -0300

    test: ensure JwtAdapter calls with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+import { Encrypter } from '../../../data/protocols/criptography/encrypter'
+import { JwtAdapter } from './jwt-adapter'
+
+interface SutTypes {
+  sut: Encrypter
+  secret: string
+}
+
+const makeSut = (): SutTypes => {
+  const secret = 'secret'
+  const sut = new JwtAdapter(secret)
+  return {
+    sut,
+    secret
+  }
+}
+
+describe('Jwt Adapter', () => {
+  test('Should call ', async () => {
+    const { sut, secret } = makeSut()
+    const signSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt('any_id')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, secret)
+  })
+})
