02da19ef9b703e8a0a298670874b706fc7d6e545
Bruno Mendes <bruno.mendes51@hotmail.com>
2020-11-27 15:23:33 -0300

    refactor: separate factories in folders

--- src/main/factories/controllers/signup/signup-factory.ts
+++ src/main/factories/controllers/signup/signup-factory.ts
@@ -1,24 +1,11 @@
 import { SignUpController } from '../../../../presentations/controllers/signup/signup-controller'
-import { DbAddAccount } from '../../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
-import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
-import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
 import { Controller } from '../../../../presentations/protocols'
-import { LogMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
 import { makeSignUpValidation } from './signup-validation-factory'
-import { JwtAdapter } from '../../../../infra/criptography/jwt-adapter/jwt-adapter'
-import env from '../../../config/env'
-import { DbAuthentication } from '../../../../data/usecases/authentication/db-authentication'
+import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
+import { makeAddAccount } from '../../usecases/add-account/add-account-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 
 export const makeSignUpController = (): Controller => {
-  const salt = 12
-  const bcryptAdapter = new BcryptAdapter(salt)
-  const jwtAdapter = new JwtAdapter(env.jwtSecret)
-  const accountMongoRepository = new AccountMongoRepository()
-  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const validationComposite = makeSignUpValidation()
-  const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(dbAddAccount, validationComposite, dbAuthentication)
-  const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(signUpController, logMongoRepository)
+  const controller = new SignUpController(makeAddAccount(), makeSignUpValidation(), makeDbAuthentication())
+  return makeLogControllerDecorator(controller)
 }
---
+++ src/main/factories/usecases/authentication/db-authentication-factory.spec.ts
@@ -0,0 +1,12 @@
+import { DbAuthentication } from '../../../../data/usecases/authentication/db-authentication'
+import { makeDbAuthentication } from './db-authentication-factory'
+
+jest.mock('../../../../data/usecases/authentication/db-authentication')
+
+describe('DbAuthentication Factory', () => {
+  test('Should call DbAuthentication with all dependencies', () => {
+    makeDbAuthentication()
+    expect(DbAuthentication).toHaveBeenCalledWith(
+      expect.any(Object), expect.any(Object), expect.any(Object), expect.any(Object))
+  })
+})
