2c3d8a5b55e84b4744145ef4b19487af70d0b7dd
Bruno Mendes <bruno.mendes51@hotmail.com>
2020-12-01 08:44:04 -0300

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,12 +1,29 @@
 import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
+import { AddSurveyModel } from '../../domain/usecases/add-survey'
+import { AddAccountModel } from '../../domain/usecases/add-account'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 import env from '../config/env'
 
 let surveyCollection: Collection
 let accountCollection: Collection
+
+const makeFakeSurveyData = (): AddSurveyModel => ({
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }],
+  date: new Date()
+})
+
+const makeFakeAccount = (): AddAccountModel => ({
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'hashed_password'
+})
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -73,10 +90,27 @@ describe('Login Routes', () => {
     })
   })
   describe('GET /surveys', () => {
-    test('Should return 403 on try load survey without token', async () => {
+    test('Should return 403 on load surveys without token', async () => {
       await request(app)
         .get('/api/surveys')
         .expect(403)
+    })
+    test('Should return 200 on load surveys with token', async () => {
+      await surveyCollection.insertOne(makeFakeSurveyData())
+      const res = await accountCollection.insertOne(makeFakeAccount())
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200)
     })
   })
 })
