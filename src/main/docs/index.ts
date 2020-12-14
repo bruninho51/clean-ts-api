8c38cddd0d322afe20688e29342426071012efe9
Bruno Mendes <bruno.mendes51@hotmail.com>
2020-12-14 16:15:58 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,10 @@
+import { title } from 'process'
+
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API para realizar enquetes entre programadores',
+    version: '1.0.0'
+  }
+}
