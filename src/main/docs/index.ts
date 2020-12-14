a815442ed1f51e469cb93586de4c70b2d63f415d
Bruno Mendes <bruno.mendes51@hotmail.com>
2020-12-14 17:15:23 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,4 +1,6 @@
-import { title } from 'process'
+import { loginPath } from '@/main/docs/paths/login-path'
+import { accountSchema } from '@/main/docs/schemas/account-schema'
+import { loginParamsSchema } from '@/main/docs/schemas/login-params-schema'
 
 export default {
   openapi: '3.0.0',
@@ -6,5 +8,18 @@ export default {
     title: 'Clean Node API',
     description: 'API para realizar enquetes entre programadores',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
