e37c5cd3f45ef994ac15fd556761f62a9673b68a
Bruno Mendes <bruno.mendes51@hotmail.com>
2020-12-17 17:43:53 -0300

    docs: add signUp api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,11 +1,13 @@
 import { loginPath } from '@/main/docs/paths/login-path'
 import { badRequest, unauthorized, serverError, notFound, forbidden } from '@/main/docs/components'
 import { surveysPath } from './paths/surveys-path'
+import { signUpPath } from './paths/signup-path'
 import {
   accountSchema,
   apiKeyAuthSchema,
   errorSchema,
   loginParamsSchema,
+  signUpParamsSchema,
   surveyAnswerSchema,
   surveySchema,
   surveysSchema
@@ -28,7 +30,8 @@ export default {
   }],
   paths: {
     '/login': loginPath,
-    '/surveys': surveysPath
+    '/surveys': surveysPath,
+    '/signup': signUpPath
   },
   schemas: {
     account: accountSchema,
@@ -36,7 +39,8 @@ export default {
     error: errorSchema,
     surveyAnswer: surveyAnswerSchema,
     survey: surveySchema,
-    surveys: surveysSchema
+    surveys: surveysSchema,
+    signUpParams: signUpParamsSchema
 
   },
   components: {
