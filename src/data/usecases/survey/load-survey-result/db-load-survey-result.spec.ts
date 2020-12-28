df3763e0a7e1ec739ae2b9c882eefee15af48d6e
Bruno Mendes <bruno.mendes51@hotmail.com>
2020-12-28 14:42:23 -0300

    test: ensure DbLoadSurveyResult returns a surveyResult on success

--- src/data/usecases/survey/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey/load-survey-result/db-load-survey-result.spec.ts
@@ -1,4 +1,5 @@
 import { makeLoadSurveyResultRepository } from '@/data/test'
+import { throwError } from '@/domain/test'
 import { DbLoadSurveyResult } from './db-load-survey-result'
 import { LoadSurveyResultRepository } from './db-load-survey-result-protocols'
 
@@ -23,5 +24,16 @@ describe('DbLoadSurveyResult UseCase', () => {
     const loadSurveyByIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
     await sut.load('any_survey_id')
     expect(loadSurveyByIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+  test('Should throw if LoadSurveyResultRepository throws', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockImplementationOnce(throwError)
+    const promise = sut.load('any_survey_id')
+    await expect(promise).rejects.toThrow()
+  })
+  test('Should return SurveyResultModel on success', async () => {
+    const { sut } = makeSut()
+    const surveyResultModel = await sut.load('any_survey_id')
+    expect(surveyResultModel.surveyId).toBe('any_survey_id')
   })
 })
